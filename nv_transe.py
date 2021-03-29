b05b0cf88486cab83444b6e2cc18b4255ff01b95
Your Name <you@example.com>
2021-03-29 20:10:51 +0800

    second

--- nv_transe.py
+++ nv_transe.py
@@ -1,26 +1,57 @@
+#!/usr/bin/python3
 import configparser
-config = configparser.ConfigParser()
 
 NV_PATH = 'mib-home-fap.nv'
-
 SEC_BASE = 'FAP.0.ETHERNET_INTERFACE'
-SEC_INTERFACE = SEC_BASE + '.' + 'ETHERNET_INTERFACE'
 
-SEC_VLAN=
+INTFSEC = [ SEC_BASE, '.']
+print(INTFSEC)
 
+#SEC_VLAN=
 
+config = configparser.ConfigParser()
 config.read(NV_PATH)
 value = config['FAP.0.ETHERNET_INTERFACE/0']['INTERFACE_ENABLE/0']
-print(value)
+#print(value)
 
 value = config['FAP.0.ETHERNET_INTERFACE/0']['INTERFACE_MAC_ADDRESS/0']
-print(value)
-
-
-#for i in
+#print(value)
 
 #class AddressConfig:
-#
-#class InterfaceConfig:
+
+str = 'FAP.0.ETHERNET_INTERFACE/0'
+print(str.split('/')[-2])
+#exit()
+
+class InterfaceConfig:
+	def __init__(self, index):
+		self.index = index
+		self.enable = '1'
+		self.name = 'wangchao'
+		self.maxbitrate = '250'
+		self.duplexmode = ['Half', 'Full', 'Auto']
 
 
+#		self.index = self.get_index
+#		self.enable = 
+#		self.name = 
+#		self.status
+	def get_from_nv(self):
+		self.enable = '1'
+		self.name = 'wangchao'
+		self.maxbitrate = '250'
+		self.duplexmode = ['Half', 'Full', 'Auto']
+
+interface = list()
+for index in range(10):
+	interface.append(InterfaceConfig(index))
+print(interface[8].name)
+
+#index
+#enable
+#name
+#status
+#macaddress
+#maxbitrate
+#signtransmedia
+#duplexmode
